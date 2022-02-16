3dbac0da491552aa1684703a331bcf8a7e3fc3a9
AlbertoDelPozo <72515390+AlbertoDelPozo@users.noreply.github.com>
2022-02-16 17:49:36 +0100

    Redirecciones para los a href

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
-    #[Route('/login', name: 'login')]
+    #[Route('/', name: 'login')]
 
     public function index(AuthenticationUtils $authenticationUtils): Response {
         // get the login error if there is one
@@ -17,7 +17,6 @@ class LoginController extends AbstractController
 
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
-
 
         return $this->render('login/index.html.twig', [
             'last_username' => $lastUsername,
