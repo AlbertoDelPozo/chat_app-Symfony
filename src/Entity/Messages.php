206eae181e409f7f9f9f856f6bfa5e255cdd8fc7
AlbertoDelPozo <72515390+AlbertoDelPozo@users.noreply.github.com>
2022-02-21 10:57:51 +0100

    mensajes

---
+++ src/Entity/Messages.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Messages
+ *
+ * @ORM\Table(name="messages")
+ * @ORM\Entity
+ */
+class Messages
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="message", type="string", length=255, nullable=false)
+     */
+    private $message;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime", nullable=false, options={"default"="CURRENT_TIMESTAMP"})
+     */
+    private $date = 'CURRENT_TIMESTAMP';
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="isRead", type="boolean", nullable=false)
+     */
+    private $isread;
+
+
+}
