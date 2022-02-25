7ccc1624272d1c39ac5e63ee1af0ab4e9896cf9f
AlbertoDelPozo <72515390+AlbertoDelPozo@users.noreply.github.com>
2022-02-25 13:27:44 +0100

    intentando arreglar el perfil

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -7,31 +7,37 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\UserRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use App\Service\FileUploader;
+use Symfony\Component\HttpFoundation\Request;
 
 
 class ProfileController extends AbstractController
 {
     #[Route('/profile', name: 'profile')]
-    public function index(ManagerRegistry $doctrine): Response
+    public function index(ManagerRegistry $doctrine, Request $request, FileUploader $fileUploader): Response
     {
         /** @var \App\Entity\User $user */
         $user = $this->getUser();
         $entityManager = $doctrine->getManager();
 
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            
+
             $user->setTlf($_POST['changeTlf']);
-            if(isset($_POST['fileToUpload']) && $_POST['fileToUpload'] !== '') {
-                $user->setProfileImage($_POST['fileToUpload']);
-            } 
 
-            //Save changes in database
-            $entityManager->persist($user);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('profile');
+            /** @var UploadedFile $brochureFile */
+            $brochureFile = $_FILES["fileToUpload"]["name"];
+            if ($brochureFile) {
+                $brochureFileName = $fileUploader->upload($_FILES['fileToUpload']['name']);
+                $user->setProfileImage($brochureFileName);
+            }
         }
-            
+        //Save changes in database
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('profile');
+
+
 
         return $this->render('profile/index.html.twig', [
             'image' => $user->getProfileImage(),
